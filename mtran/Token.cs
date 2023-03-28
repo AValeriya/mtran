400c861c6f4968208b27af7f0e615e288386f401
AValeriya <79207820+AValeriya@users.noreply.github.com>
2023-03-29 02:35:51 +0300

    indentation

--- mtran/Token.cs
+++ mtran/Token.cs
@@ -1,19 +1,25 @@
-﻿namespace mtran
+namespace mtran
 {
 	internal class Token
 	{
 		internal Lexer lexer;
 		internal LexemType type;
+		internal int line;
+		internal int row;
 		internal char symbol;
+		internal int indentation;
 		internal int nameIndex;
 		internal int constIndex;
 		internal int keywordIndex;
 
-		internal Token(Lexer lexer, LexemType type, char symbol = '\0', int nameIndex = -1, int constIndex = -1, int keywordIndex = -1)
+		internal Token(Lexer lexer, LexemType type, int line, int row, char symbol = '\0', int indentation = 0, int nameIndex = -1, int constIndex = -1, int keywordIndex = -1)
 		{
 			this.lexer = lexer;
 			this.type = type;
+			this.line = line;
+			this.row = row;
 			this.symbol = symbol;
+			this.indentation = indentation;
 			this.nameIndex = nameIndex;
 			this.constIndex = constIndex;
 			this.keywordIndex = keywordIndex;
